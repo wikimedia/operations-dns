31bbf82393d6bc8f52ad89cb62adb169bde9a71d
Brandon Black <bblack@wikimedia.org>
2018-12-10 23:26:15 +0000

    authdns config/CI refactor [4/5]
    Remove now-useless config-geo-test.
    
    Add authdns-gen-zones.py (unchanged) from the puppet repo to
    utils/, which the authdns-lint scripts should switch to use in a
    future commit.
    
    Add check-gdnsd.sh to utils/.  This is a rework of puppet's
    authdns-lint, but simplified and assumes it runs from the repo
    root, and its only optional argument is the test output directory.
    
    Add a README file to utils/ about all the things there.
    
    In theory at this point, one can do full CI (the gdnsd checkconf
    and zones_validator) from stretch using just the ops/dns repo and
    nothing from puppet, assuming all the right packages are
    installed (python, jinja2, python3-3.5+, gdnsd), by executing a
    short shell script that looks something like:
    
        set -e
        cd $REPO_ROOT
        utils/check-gdnsd.sh
        utils/zone_validator.py
    
    We should be able to wrap that up in a Dockerfile pretty easily!
    
    In the meantime, we can also switch the puppet repo's existing
    authdns-lint into a wrapper over utils/check-gdnsd.sh from here
    that still works on jessie.
    
    Bug: T205439
    Change-Id: Ie85432eb097dd6254888409261abb26cc8846081

---
+++ utils/check-gdnsd.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Shell script to use for linting zone templates & config. It sets up a gdnsd
+# etc directory, generates zone files based on the templates using
+# authdns-gen-zones and runs gdnsd checkconf.
+#
+# Written by Faidon Liambotis, Aug 2013
+
+set -e
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+die() { echo >&2 "E: $*"; exit 1; }
+
+cleanup() {
+   if [ "$CLEANUP" = "yes" ] && [ -d "$TESTDIR" ]; then
+       rm -rf $TESTDIR
+   fi
+}
+trap cleanup EXIT
+
+if [ ! -e templates/wikimedia.org ]; then
+    die "must be executed from the DNS repo root"
+fi
+
+TESTDIR=$1
+if [ -z "$TESTDIR" ]; then
+    # no test directory passed, generate one and clean it up on exit
+    TESTDIR=$(mktemp -d --tmpdir 'authdns-lint.XXXXXX')
+    CLEANUP=yes
+fi
+
+echo "Using $TESTDIR as the output working directory (gdnsd etc)"
+mkdir -p $TESTDIR/zones
+mkdir -p $TESTDIR/geoip
+
+echo "Generating zonefiles from zone templates"
+utils/authdns-gen-zones.py templates $TESTDIR/zones
+
+echo "Generating gdnsd config"
+for realf in config geo-maps geo-resources; do
+    cp -f $realf $TESTDIR/
+done
+for mockf in config-options discovery-map discovery-geo-resources discovery-metafo-resources discovery-states; do
+    cp -f utils/mock_etc/$mockf $TESTDIR/
+done
+cp -f utils/mock_etc/geoip/GeoIP2-City.mmdb $TESTDIR/geoip/
+
+gdnsd -sSc $TESTDIR checkconf
